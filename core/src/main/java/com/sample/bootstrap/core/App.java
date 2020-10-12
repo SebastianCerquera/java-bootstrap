e782ebd1c4d3f2ffa2716db33e54d6094cb8b29b
Sebastian Gonzalez <sebastian2zen@gmail.com>
2020-10-12 15:25:33 -0500

    handling other threads exceptions

--- core/src/main/java/com/sample/bootstrap/core/App.java
+++ core/src/main/java/com/sample/bootstrap/core/App.java
@@ -1,8 +1,43 @@
 package com.sample.bootstrap.core;
 
 public class App {
+	
+	public static void handleUncaughtException() {
+		
+		Thread.UncaughtExceptionHandler handler = new Thread.UncaughtExceptionHandler() {
+		    public void uncaughtException(Thread th, Throwable ex) {
+		        System.out.println("Uncaught exception: " + ex);
+		    }
+		};
+
+		// create another thread
+		Thread otherThread = new Thread() {
+		    public void run() {
+		        System.out.println("Sleeping ...");
+		        try {
+		            Thread.sleep(60000);
+		        } catch (InterruptedException e) {
+		            System.out.println("Interrupted.");
+		        }
+		        System.out.println("Throwing exception ...");
+		        throw new RuntimeException();
+		    }
+		};
+		
+		// start the other thread - our uncaught exception handler will be invoked when
+		// the other thread throws an uncaught exception
+		otherThread.start();
+		
+		// set our uncaught exception handler as the one to be used when the new thread
+		// throws an uncaught exception
+		otherThread.setUncaughtExceptionHandler(handler);
+		
+	}
     
     public static void main(String[] args) {
+    	
+    	// A thread can delegate the exception handler to its creator thread.
+    	handleUncaughtException();
     }
     
 }
